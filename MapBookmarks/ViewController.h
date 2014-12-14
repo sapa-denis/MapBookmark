784d5ea29c725f5f1122a8044736f31fb87939b7
Sapa Denys <sapa.denis@gmail.com>
2014-12-14 11:32:41 +0200

    Initial Commit

---
+++ MapBookmarks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapBookmarks
+//
+//  Created by Sapa Denys on 14.12.14.
+//  Copyright (c) 2014 Sapa Denys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
